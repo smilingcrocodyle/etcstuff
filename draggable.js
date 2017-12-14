10289e4d7781d3a9ad6b3f08db1a8f99cf19865a
Smiling Crocodyle <werindependente@gmail.com>
2017-12-14 13:26:57 +0300

    ...

--- draggable.js
+++ draggable.js
@@ -1,65 +1,67 @@
 //1. Сделать так, чтобы mousedown не реагировала на простые клики (только на перетаскивание).
 //2. Разобраться с позиционированием элемента (за что ухватили - за то и тащим: https://learn.javascript.ru/drag-and-drop).
 
-var draggable;
-
-function Draggable(event) {
-    var coords = getCoords(event.target);
-    var dropplace = document.getElementById('drop');
-    var dropcoords = getCoords(dropplace);
-    this.element = {
-        element: event.target,
-        elementY: coords.top,
-        elementX: coords.left
-    };
+function Draggable() {
+    var dropplace = document.getElementById('drop'),
+    dropcoords = Draggable.getCoords(dropplace);
     this.dropfield = {
         dropY: dropcoords.top,
         dropX: dropcoords.left,
         dropW: dropplace.offsetWidth,
         dropH: dropplace.offsetHeight
     };
-
-    document.onmousemove = (event = event || window.event) => { draggable.move(); };
-
-    function getCoords(element) {
-        var rec = element.getBoundingClientRect();
-        var recTopScroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
-        var recLeftScroll = window.scrollX || document.documentElement.scrollLeft || document.body.scrollLeft;
-        return {
-            top: rec.top + recTopScroll,
-            left: rec.left + recLeftScroll
-        };
-    };
+    this.started = false;
 };
 
-Draggable.prototype.move = function() {
+Draggable.prototype.start = function(event) {
+    if (event.which != 1) return;
+    if (!event.target.classList.contains('item')) return;
+    var coords = Draggable.getCoords(event.target);
+    this.element = {
+        element: event.target,
+        elementY: coords.top,
+        elementX: coords.left };
+    this.started = true;
+}
+
+Draggable.prototype.move = function(event) {
+    if (!this.started) return;
     this.element.element.style.position = 'fixed';
     this.element.element.style.zIndex = '1000';
 
-    var elementTop = event.clientY - this.element.element.offsetHeight/2;
-    var elementLeft = event.clientX - this.element.element.offsetWidth/2;
+    var elementTop = event.clientY,
+    elementLeft = event.clientX;
     this.element.element.style.top = elementTop + 'px';
     this.element.element.style.left = elementLeft + 'px';
+};
 
-    document.onmouseup = (event = event || window.event) => {
-        switch (true) {
-            case event.clientX < this.dropfield.dropX:
-            case event.clientX > this.dropfield.dropX + this.dropfield.dropW:
-            case event.clientY < this.dropfield.dropY:
-            case event.clientY > this.dropfield.dropY + this.dropfield.dropH:
-                this.element.element.style.top = this.element.elementY + 'px';
-                this.element.element.style.left = this.element.elementX + 'px';
-                this.element.element.style.position = 'static';
-                break;
-        };
-        this.element.element.style.zIndex = 'auto';
-        draggable = document.onmouseup = document.onmousemove = null;
+Draggable.prototype.stop = function(event) {
+    switch (true) {
+        case event.clientX < this.dropfield.dropX:
+        case event.clientX > this.dropfield.dropX + this.dropfield.dropW:
+        case event.clientY < this.dropfield.dropY:
+        case event.clientY > this.dropfield.dropY + this.dropfield.dropH:
+            this.element.element.style.top = this.element.elementY + 'px';
+            this.element.element.style.left = this.element.elementX + 'px';
+            this.element.element.style.position = 'static';
+            break;
+    };
+    this.element.element.style.zIndex = 'auto';
+    this.element = null; this.started = false;
+};
+
+Draggable.getCoords = function(element) {
+    var rec = element.getBoundingClientRect(),
+    recTopScroll = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop,
+    recLeftScroll = window.scrollX || document.documentElement.scrollLeft || document.body.scrollLeft;
+    return {
+        top: rec.top + recTopScroll,
+        left: rec.left + recLeftScroll
     };
 };
 
-document.onmousedown = (event = event || window.event) => {
-    if (event.which != 1) return;
-    if (!event.target.classList.contains('item')) return;
-    draggable = new Draggable(event);
-    return false;
-};
+var draggable = new Draggable();
+
+document.onmousedown = (event = event || window.event) => { draggable.start(event); return false; };
+document.onmousemove = (event = event || window.event) => { draggable.move(event); };
+document.onmouseup = (event = event || window.event) => { draggable.stop(event); };
